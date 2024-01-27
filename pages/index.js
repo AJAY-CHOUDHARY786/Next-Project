2e754023f0af5a16eba63e3db50a9aa0551ba59e
AJAY-CHOUDHARY786 <ak8032161@gmail.com>
2024-01-27 15:01:51 +0530

    n

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,6 @@
-import Login from "./Login";
-import AdminPanel from "./AdminPanel";
-import Sidebar from "./Sidebar";
-import Headerr from "./Headerr";
-import DoubtPage from "./DoubtPage";
+
 import NewDoubt from "./NewDoubt";
-import Header from "../component/header";
-import AnimatedText from "../component/AnimatedText";
 
-
-// import Login from "../component/Login";
-//  import Catform from "../component/Catform";
 
 
 export default function Home() { 
